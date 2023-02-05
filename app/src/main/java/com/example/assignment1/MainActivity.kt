b798cc913d5bcac37101c08cd481a41867be5d61
Edwardd02 <tup19197@temple.edu>
2023-02-05 13:50:10 +0900

    Assignment01

--- app/src/main/java/com/example/assignment1/MainActivity.kt
+++ app/src/main/java/com/example/assignment1/MainActivity.kt
@@ -1,11 +1,17 @@
 package com.example.assignment1
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val textView = findViewById<TextView>(R.id.textview)
+        findViewById<View>(R.id.button).setOnClickListener {textView.text = "Clicked!"}
     }
 }
